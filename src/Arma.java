dadf4a856b37b74cf7479a436d0889c939937e31
Fagner Louis <faa.louis@hotmail.com>
2025-08-28 19:31:16 -0300

    Primeiro commit, classes da musica

---
+++ src/Arma.java
@@ -0,0 +1,16 @@
+public class Arma {
+
+    String modelo;
+    int qtdtiros;
+    String dono;
+
+    public void atirar(int tiros) {
+        System.out.println("A arma disparou " + tiros + " tiros");
+    }
+
+    public static void main(String[] args) {
+        Arma winchester22 = new Arma();
+        winchester22.atirar(5);
+    }
+
+}
---
+++ src/Cidade.java
@@ -0,0 +1,19 @@
+public class Cidade {
+
+    String nome;
+    String regiao;
+    boolean eViolenta;
+
+    public void transformar() {
+        System.out.println("A cidade transformou a pessoa.");
+    }
+
+    public static void main(String[] args) {
+        Cidade brasilia = new Cidade();
+        brasilia.nome = "Brasilia";
+        brasilia.regiao = "Plano Piloto";
+        brasilia.eViolenta = true;
+        brasilia.transformar();
+    }
+
+}
