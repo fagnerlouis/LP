a2bf158fe790f15191275e38b883c6256b06e064
Fagner Louis <faa.louis@hotmail.com>
2025-09-04 14:10:15 -0300

    separação das mensagens no console para facilitar a leitura

--- src/Main.java
+++ src/Main.java
@@ -21,15 +21,23 @@ import Video.Camera;
 public class Main {
     public static void main(String[] args) {
         // -------- Música (classe aparece na letra) --------
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Música: Faroeste Caboclo");
+        System.out.println();
         Personagem joao = new Personagem("João de Santo Cristo", "Interior", true);
         Cidade brasilia = new Cidade("Brasília", "Plano Piloto", true);
-        Arma winchester = new Arma("Winchester .22", 6, "João");
+        Arma winchester = new Arma("Winchester .22", 5, "João");
 
         joao.viajar("Brasília");
         brasilia.transformar();
         winchester.atirar(5);
+        System.out.println();
+        System.out.println();
 
         // -------- Imagem Vik Muniz: Lampedusa --------
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Imagem Vik Muniz: Lampedusa");
+        System.out.println();
         Barco barcoDePapel = new Barco("papel", 5.0, 2);
         ImagemVikMuniz.img1Lampedusa.Cidade veneza = new ImagemVikMuniz.img1Lampedusa.Cidade("Veneza", 3000, "Itália");
         Jornal jornal = new Jornal("Centinaia di morti", "Italiano", 24);
@@ -37,8 +45,29 @@ public class Main {
         barcoDePapel.navegar();
         veneza.descricao();
         jornal.exibirNoticia();
+        System.out.println();
+        System.out.println();
+
+        // -------- Imagem Vik Muniz: Track Brawl --------
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Imagem Vik Muniz: Track Brawl");
+        System.out.println();
+        Trilho trilho = new Trilho("aço", 100.0, true);
+        Lutador homem1 = new Lutador("Homem A", 80, false);
+        Lutador homem2 = new Lutador("Homem B", 75, false);
+        Mala mala = new Mala("marrom", "couro", false);
+
+        trilho.transportar();
+        homem1.lutar();
+        homem2.lutar();
+        mala.abrir();
+        System.out.println();
+        System.out.println();
 
         // -------- Doodle --------
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Doodle");
+        System.out.println();
         Astronauta astro = new Astronauta("Neil", "Exploração Lunar", 70);
         Perifericos mouse = new Perifericos("Mouse", "Logitech", false);
         Robo robo = new Robo("RX-78", 50, true);
@@ -54,19 +83,14 @@ public class Main {
         robo.apresentar();
         robo.executarTarefa();
         robo.recarregar();
+        System.out.println();
+        System.out.println();
 
-        // -------- Imagem Vik Muniz: Track Brawl --------
-        Trilho trilho = new Trilho("aço", 100.0, true);
-        Lutador homem1 = new Lutador("Homem A", 80, false);
-        Lutador homem2 = new Lutador("Homem B", 75, false);
-        Mala mala = new Mala("marrom", "couro", false);
-
-        trilho.transportar();
-        homem1.lutar();
-        homem2.lutar();
-        mala.abrir();
 
         // -------- Vídeo --------
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Vídeo Downhill de bicicleta");
+        System.out.println();
         Bicicleta bike = new Bicicleta("Alumínio 6061", 21, 29);
         Capacete capacete = new Capacete("M", "ABS", false);
         Camera camera = new Camera("4K", 60, false);
@@ -82,6 +106,8 @@ public class Main {
         camera.gravar();
         camera.parar();
         camera.trocarBateria();
+        System.out.println();
+        System.out.println();
 
     }
 }
